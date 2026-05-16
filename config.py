59c2b6f3902f748c329c8e305d0b0b91bb4a3780
Ahmet <ahmetkucukyaman2006@gmail.com>
2026-05-16 14:37:56 +0300

    Config: Google SMTP sunucu ayarları ve çevresel değişkenler bağlandı.

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+# .env dosyasındaki çevresel değişkenleri yükle
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Mail Ayarları
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
